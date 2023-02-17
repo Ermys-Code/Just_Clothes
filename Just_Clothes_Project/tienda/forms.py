8fe8dbe573a05c07151fa064a82c8ccf6984f924
Raúl Rodríguez Trotonda <raul.rodriguez.trotonda@gmail.com>
2023-02-17 13:06:20 +0100

    form edit user

---
+++ Just_Clothes_Project/tienda/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+
+class EditProfile(forms.Form):
+    username = forms.CharField(max_length=1000, widget=forms.TextInput(attrs={'class': "form-control"}))
+    first_name = forms.CharField(max_length=1000, widget=forms.TextInput(attrs={'class': "form-control"}))
+    last_name = forms.CharField(max_length=1000, widget=forms.TextInput(attrs={'class': "form-control"}))
+    email = forms.EmailField(max_length=200, widget=forms.EmailInput(attrs={'class': "form-control"}))
+    address = forms.CharField(max_length=1000, widget=forms.TextInput(attrs={'class': "form-control"}))
